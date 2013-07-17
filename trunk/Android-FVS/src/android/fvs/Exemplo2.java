e61bd7262a7446348b3cef5bf2c87e12b92597a5
placido.fvs <placido.fvs@d32e132b-2dff-8f96-cabd-66d971926318>
2013-07-17 23:21:35 +0000

    Commit inicial

---
+++ trunk/Android-FVS/src/android/fvs/Exemplo2.java
@@ -0,0 +1,12 @@
+package android.fvs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Exemplo2 extends Activity {
+	
+	public void onCreate(Bundle ex2){
+		super.onCreate(ex2);
+		setContentView(R.layout.layout_exemplo2);
+	}
+}
