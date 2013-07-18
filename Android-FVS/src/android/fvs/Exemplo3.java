1ee1feb5a2d15130a62bd3fe5780f6483e783303
placido.fvs <placido.fvs@d32e132b-2dff-8f96-cabd-66d971926318>
2013-07-18 23:04:38 +0000

    Commit aula 3

--- Android-FVS/src/android/fvs/Exemplo3.java
+++ Android-FVS/src/android/fvs/Exemplo3.java
@@ -2,13 +2,16 @@ package android.fvs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Exemplo3 extends Activity {
 	
-	public void onCreate(Bundle ex2){
-		super.onCreate(ex2);
-		setContentView(R.layout.layout_exemplo2);
+	public void onCreate(Bundle ex3){
+		super.onCreate(ex3);
+		//abre a tela layout_exemplo3.xml
+		setContentView(R.layout.layout_exemplo3);
+		//Busca a imagem pelo id
+		ImageView img2 = (ImageView) findViewById(R.id.imagem2);
+		img2.setImageResource(R.drawable.android2);
 	}
 }
-//ImageView img2 = (ImageView) findViewById(R.id.imagem2);
-//img2.setImageResource(R.drwable.android2);
