070ed2c5e3c8edc997b81f74199be65f2d6847cb
placido.fvs <placido.fvs@d32e132b-2dff-8f96-cabd-66d971926318>
2013-07-18 01:02:19 +0000

    Commit aula 2.1.1

--- Android-FVS/src/android/fvs/Exemplo3.java
+++ Android-FVS/src/android/fvs/Exemplo3.java
@@ -3,10 +3,12 @@ package android.fvs;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Exemplo2 extends Activity {
+public class Exemplo3 extends Activity {
 	
 	public void onCreate(Bundle ex2){
 		super.onCreate(ex2);
 		setContentView(R.layout.layout_exemplo2);
 	}
 }
+//ImageView img2 = (ImageView) findViewById(R.id.imagem2);
+//img2.setImageResource(R.drwable.android2);
