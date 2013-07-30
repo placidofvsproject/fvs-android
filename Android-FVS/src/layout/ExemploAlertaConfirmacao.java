af9b768f7856a0f9660bd3733c6a8f39db04c527
placido.fvs <placido.fvs@d32e132b-2dff-8f96-cabd-66d971926318>
2013-07-30 23:51:41 +0000

    aula 10

---
+++ Android-FVS/src/layout/ExemploAlertaConfirmacao.java
@@ -0,0 +1,43 @@
+package layout;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.fvs.R;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class ExemploAlertaConfirmacao extends Activity implements
+		OnClickListener {
+
+	Button b;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.alert_dialog);
+		b = (Button) findViewById(R.id.btAlertar);
+		b.setOnClickListener(this);
+	}
+	
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+		alerta.setIcon(R.drawable.android1);
+		alerta.setTitle("Título");
+		alerta.setMessage("Por favor escolha sim ou não!");
+		alerta.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				Toast.makeText(ExemploAlertaConfirmacao.this, "Clicou em Sim!", Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+
+}
