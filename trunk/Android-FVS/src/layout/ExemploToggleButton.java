27dc1ee149bc6fbb0cf8d7bfdafb1c4ba1e60532
placido.fvs <placido.fvs@d32e132b-2dff-8f96-cabd-66d971926318>
2013-07-30 00:36:51 +0000

    aula 9.1

---
+++ trunk/Android-FVS/src/layout/ExemploToggleButton.java
@@ -0,0 +1,15 @@
+package layout;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class ExemploToggleButton extends Activity implements OnClickListener {
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
