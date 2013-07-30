05ac9c19ebd708a30ae63608d1ab5db10432b9d7
placido.fvs <placido.fvs@d32e132b-2dff-8f96-cabd-66d971926318>
2013-07-30 00:50:37 +0000

    aula 9.2

--- trunk/Android-FVS/src/layout/ExemploToggleButton.java
+++ trunk/Android-FVS/src/layout/ExemploToggleButton.java
@@ -1,15 +1,31 @@
 package layout;
 
 import android.app.Activity;
+import android.fvs.R;
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class ExemploToggleButton extends Activity implements OnClickListener {
 
+	ToggleButton toggle;
+	Button b;
+	public void onClick(Bundle icicle){
+		super.onCreate(icicle);
+		setContentView(R.layout.exemplo_toggle_button);
+		toggle = (ToggleButton) findViewById(R.id.toggle);
+		b = (Button) findViewById(R.id.btOk);
+		b.setOnClickListener(this);
+	}
+	
 	@Override
 	public void onClick(View arg0) {
 		// TODO Auto-generated method stub
-
+		//if(arg0==b){}
+		Toast.makeText(this, "Selecionado: "+toggle.isChecked(), Toast.LENGTH_LONG).show();
 	}
 
 }
